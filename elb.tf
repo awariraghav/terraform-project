f9816bab435f799e27151b80971aabaa499cd254
awariraghav <126005025+awariraghav@users.noreply.github.com>
2023-04-16 08:20:24 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -24,15 +24,3 @@ resource "aws_elb" "bar" {
     Name = "raham-tf-elb"
   }
 }
-
-resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "aurora-cluster-demo"
-  engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.11.2"
-  availability_zones      = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  database_name           = "mydb"
-  master_username         = "foo"
-  master_password         = "Rahamshaik#444555"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-}
