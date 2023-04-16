1fd0306442834a997fc7eeddd2837b6976d0e338
awariraghav <126005025+awariraghav@users.noreply.github.com>
2023-04-16 08:13:56 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ EOF
 resource "aws_instance" "two" {
   ami             = "ami-0d81306eddc614a45"
   instance_type   = "t2.micro"
-  key_name        = "rrraham"
+  key_name        = "raghavender1"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-south-1b"
   user_data       = <<EOF
@@ -46,7 +46,7 @@ EOF
 resource "aws_instance" "three" {
   ami             = "ami-0d81306eddc614a45"
   instance_type   = "t2.micro"
-  key_name        = "rrraham"
+  key_name        = "raghavender1"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "ap-south-1b"
   tags = {
